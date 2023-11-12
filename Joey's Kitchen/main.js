492ee07a10695d33278d5473814b0dd08b255f74
Joseph Adaeghahi Musa-Baba <133718177+JosephAdaeghahiMusaBaba12@users.noreply.github.com>
2023-11-11 17:39:59 -0800

    Add files via upload

---
+++ Joey's Kitchen/main.js
@@ -0,0 +1,11 @@
+//mobile menu
+//document.addEventListener('DOMContentLoaded', () =>{
+  //  const hamburgerButton = document.querySelector('.hamburger-button');
+   // const mobileMenu = document.querySelector('.mobile-menu');
+
+   // hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
+//});
+
+document.querySelector('.menu-btn').addEventListener
+        ('click',() => document.querySelector('.main-menu')
+        .classList.toggle('show'))
